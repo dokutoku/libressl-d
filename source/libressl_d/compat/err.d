738b36dc00a734b72c29e3c7ea2b245e2a84a0d6
dokutoku <3729541-dokutoku@users.noreply.gitlab.com>
2021-10-27 19:38:04 +0900

    Fixed an error that occurred in BetterC

--- source/libressl_d/compat/err.d
+++ source/libressl_d/compat/err.d
@@ -18,93 +18,94 @@ package(libressl_d):
 //#if defined(HAVE_ERR_H)
 	//#include_next <err.h>
 //#else
-	//#if defined(_MSC_VER)
-		//__declspec(noreturn)
-	//#else
-		//__attribute__((noreturn))
-	//#endif
+	version (D_BetterC) {
+	} else {
+		//#if defined(_MSC_VER)
+			//__declspec(noreturn)
+		//#else
+			//__attribute__((noreturn))
+		//#endif
+		pragma(inline, true)
+		nothrow
+		void err(int eval, const (char)* fmt, ...)
 
-	pragma(inline, true)
-	nothrow
-	void err(int eval, const (char)* fmt, ...)
+			do
+			{
+				int sverrno = core.stdc.errno.errno;
+				core.stdc.stdarg.va_list ap;
 
-		do
-		{
-			int sverrno = core.stdc.errno.errno;
-			core.stdc.stdarg.va_list ap;
+				core.stdc.stdarg.va_start(ap, fmt);
 
-			core.stdc.stdarg.va_start(ap, fmt);
+				if (fmt != null) {
+					libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
+					libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, ": ");
+				}
 
-			if (fmt != null) {
-				libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
-				libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, ": ");
+				core.stdc.stdarg.va_end(ap);
+				libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "%s\n", libressl_d.compat.string.strerror(sverrno));
+				libressl_d.compat.stdlib.exit(eval);
 			}
 
-			core.stdc.stdarg.va_end(ap);
-			libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "%s\n", libressl_d.compat.string.strerror(sverrno));
-			libressl_d.compat.stdlib.exit(eval);
-		}
+		//#if defined(_MSC_VER)
+			//__declspec(noreturn)
+		//#else
+			//__attribute__((noreturn))
+		//#endif
+		pragma(inline, true)
+		nothrow
+		void errx(int eval, const (char)* fmt, ...)
 
-	//#if defined(_MSC_VER)
-		//__declspec(noreturn)
-	//#else
-		//__attribute__((noreturn))
-	//#endif
+			do
+			{
+				core.stdc.stdarg.va_list ap;
 
-	pragma(inline, true)
-	nothrow
-	void errx(int eval, const (char)* fmt, ...)
+				core.stdc.stdarg.va_start(ap, fmt);
 
-		do
-		{
-			core.stdc.stdarg.va_list ap;
+				if (fmt != null) {
+					libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
+				}
 
-			core.stdc.stdarg.va_start(ap, fmt);
-
-			if (fmt != null) {
-				libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
+				core.stdc.stdarg.va_end(ap);
+				libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "\n");
+				libressl_d.compat.stdlib.exit(eval);
 			}
 
-			core.stdc.stdarg.va_end(ap);
-			libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "\n");
-			libressl_d.compat.stdlib.exit(eval);
-		}
+		pragma(inline, true)
+		nothrow
+		void warn(const (char)* fmt, ...)
 
-	pragma(inline, true)
-	nothrow
-	void warn(const (char)* fmt, ...)
+			do
+			{
+				int sverrno = core.stdc.errno.errno;
+				core.stdc.stdarg.va_list ap;
 
-		do
-		{
-			int sverrno = core.stdc.errno.errno;
-			core.stdc.stdarg.va_list ap;
+				core.stdc.stdarg.va_start(ap, fmt);
 
-			core.stdc.stdarg.va_start(ap, fmt);
+				if (fmt != null) {
+					libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
+					libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, ": ");
+				}
 
-			if (fmt != null) {
-				libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
-				libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, ": ");
+				core.stdc.stdarg.va_end(ap);
+				libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "%s\n", libressl_d.compat.string.strerror(sverrno));
 			}
 
-			core.stdc.stdarg.va_end(ap);
-			libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "%s\n", libressl_d.compat.string.strerror(sverrno));
-		}
+		pragma(inline, true)
+		nothrow
+		void warnx(const (char)* fmt, ...)
 
-	pragma(inline, true)
-	nothrow
-	void warnx(const (char)* fmt, ...)
+			do
+			{
+				core.stdc.stdarg.va_list ap;
 
-		do
-		{
-			core.stdc.stdarg.va_list ap;
+				core.stdc.stdarg.va_start(ap, fmt);
 
-			core.stdc.stdarg.va_start(ap, fmt);
+				if (fmt != null) {
+					libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
+				}
 
-			if (fmt != null) {
-				libressl_d.compat.stdio.vfprintf(libressl_d.compat.stdio.stderr, fmt, ap);
+				core.stdc.stdarg.va_end(ap);
+				libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "\n");
 			}
-
-			core.stdc.stdarg.va_end(ap);
-			libressl_d.compat.stdio.fprintf(libressl_d.compat.stdio.stderr, "\n");
-		}
+	}
 //#endif
