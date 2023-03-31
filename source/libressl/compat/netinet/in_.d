95716e85204243041a24d1752e0277953ef28460
dokutoku <3729541-dokutoku@users.noreply.gitlab.com>
2023-03-31 14:12:25 +0900

    Fix compat/netinet/in_.d

--- source/libressl/compat/netinet/in_.d
+++ source/libressl/compat/netinet/in_.d
@@ -14,5 +14,5 @@ public import core.sys.posix.netinet.in_;
 public import libressl.compat.win32netcompat;
 
 version (Android) {
-	alias in_port_t = core.stdc.stdint.core.stdc.stdint.uint16_t;
+	alias in_port_t = core.stdc.stdint.uint16_t;
 }
